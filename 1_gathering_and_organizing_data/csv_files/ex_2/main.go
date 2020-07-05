86142619d7ab207d0de2246b23a255c87b7ee6b3
teng <wwwsousoucom@126.com>
2020-07-05 22:03:44 +0800

    元素个数

--- 1_gathering_and_organizing_data/csv_files/ex_2/main.go
+++ 1_gathering_and_organizing_data/csv_files/ex_2/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
+func main() {
+	// Open the iris dataset file.
+	f, err := os.Open("1_gathering_and_organizing_data/csv_files/data/iris.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	// Create a new CSV reader reading from the opened file.
+	reader := csv.NewReader(f)
+	reader.FieldsPerRecord = 6
+	var rawCSVData [][]string
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		rawCSVData = append(rawCSVData, record)
+	}
+	fmt.Println(rawCSVData)
 }
