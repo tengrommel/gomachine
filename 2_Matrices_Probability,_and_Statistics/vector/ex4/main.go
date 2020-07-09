f6948a3b9aa75b0b86f48541f30f9b03825dee17
teng <wwwsousoucom@126.com>
2020-07-09 22:36:00 +0800

    norm

---
+++ 2_Matrices_Probability,_and_Statistics/vector/ex4/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gonum/blas/blas64"
+	"github.com/gonum/matrix/mat64"
+)
+
+func main() {
+	// Initialize a couple of "vectors" represented as slices.
+	vectorA := mat64.NewVector(3, []float64{11.0, 5.2, -1.3})
+	vectorB := mat64.NewVector(3, []float64{7.2, 4.2, 5.1})
+	dotProduct := mat64.Dot(vectorA, vectorB)
+	fmt.Printf("The dot product of A and B is: %0.2f\n", dotProduct)
+	vectorA.ScaleVec(1.5, vectorA)
+	fmt.Printf("Scaling A by 1.5 gives: %v\n", vectorA)
+	normB := blas64.Nrm2(3, vectorB.RawVector())
+	fmt.Printf("The norm/length of B is: %0.2f\n", normB)
+}
