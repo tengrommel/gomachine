d55252c8bdf5e3692b5272064ba4dcddfd404d2e
teng <wwwsousoucom@126.com>
2020-07-29 00:22:08 +0800

    分布式

--- code/ch4/DistributedSort/single/main.go
+++ code/ch4/DistributedSort/single/main.go
@@ -5,18 +5,62 @@ import (
 	"fmt"
 	"gomachine/code/ch4/DistributedSort/single/pipeLineMiddleWare"
 	"os"
+	"strconv"
 	"time"
 )
 
 // 本地 多线程 分布式
 
+// 分布式
+func createNetworkPipeLine(fileName string, fileSize int, chunkCount int) <-chan int {
+	file, err := os.Create(fileName)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	myPipe := pipeLineMiddleWare.RandomSource(fileSize / 8)
+	writer := bufio.NewWriter(file)
+	pipeLineMiddleWare.WriteSlink(writer, myPipe)
+	writer.Flush()
+
+	chunkSize := fileSize / chunkCount
+	sortAddr := make([]string, 0)
+	pipeLineMiddleWare.Init()
+	file, err = os.Open(fileName)
+	if err != nil {
+		panic(err)
+	}
+	for i := 0; i < chunkCount; i++ {
+		file.Seek(int64(i*chunkSize), 0) // 移动文件指针位置
+		source := pipeLineMiddleWare.ReaderSource(bufio.NewReader(file), chunkSize)
+		addr := ":" + strconv.Itoa(7000+i) // 开辟地址
+		pipeLineMiddleWare.NetWordWrite(addr, pipeLineMiddleWare.InMemorySort(source))
+		sortAddr = append(sortAddr, addr) // 地址复制
+	}
+	sortResults := make([]<-chan int, 0)
+	for _, addr := range sortAddr {
+		sortResults = append(sortResults, pipeLineMiddleWare.NetWordRead(addr))
+	}
+	return pipeLineMiddleWare.MergeN(sortResults...)
+}
+
 // 多线程 - 调用中间件完成
 func createPipeLine(fileName string, fileSize int, chunkCount int) <-chan int {
+	file, err := os.Create(fileName)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	myPipe := pipeLineMiddleWare.RandomSource(fileSize / 8)
+	writer := bufio.NewWriter(file)
+	pipeLineMiddleWare.WriteSlink(writer, myPipe)
+	writer.Flush()
+
 	chunkSize := fileSize / chunkCount   // 数量
 	sortResults := make([]<-chan int, 0) // 排序结果，一个数组，每一个元素是一个管道
 	pipeLineMiddleWare.Init()            // 初始化
 	for i := 0; i < chunkCount; i++ {
-		file, err := os.Open(fileName) // 打开文件
+		file, err = os.Open(fileName) // 打开文件
 		if err == nil {
 			panic(err)
 		}
