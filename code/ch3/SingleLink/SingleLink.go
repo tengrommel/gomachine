6893d24db542b371d199b1af2c310c53cb8b6ee3
teng <wwwsousoucom@126.com>
2020-07-28 19:56:29 +0800

    尾部插入

--- code/ch3/SingleLink/SingleLink.go
+++ code/ch3/SingleLink/SingleLink.go
@@ -4,9 +4,14 @@ import "fmt"
 
 type SingleLink interface {
 	// 增删改查
-	GetFirstNode() *Node            // 抓取头部节点
-	InsertNodeFront(node *Node)     // 头部插入
-	InsertNodeBack(node *Node)      // 尾部插入
+	GetFirstNode() *Node        // 抓取头部节点
+	InsertNodeFront(node *Node) // 头部插入
+	InsertNodeBack(node *Node)  // 尾部插入
+
+	// 在一个节点之前插入或者在一个节点之后插入
+	InsertNodeValueFront(dest interface{}, node *Node) bool // 头部插入数据
+	InsertNodeValueBack(dest interface{}, node *Node) bool  // 尾部插入数据
+
 	GetNodeAtIndex(index int) *Node // 根据索引抓取指定位置的节点
 	DeleteNode(dest *Node) bool     // 删除一个节点
 	DeleteAtIndex(index int)        // 删除指定位置的节点
@@ -17,6 +22,31 @@ type SingleLink interface {
 type List struct {
 	head   *Node // 链表的头部指针
 	length int   // 链表的长度
+}
+
+func (l *List) InsertNodeValueFront(dest interface{}, node *Node) bool {
+	pHead := l.head
+	isFind := false // 是否找到数据
+	for pHead.pNext != nil {
+		if pHead.value == dest {
+			isFind = true
+			break
+		}
+		pHead = pHead.pNext
+	}
+	if isFind {
+		// 尾部插入
+		node.pNext = pHead.pNext
+		pHead.pNext = node
+		l.length++
+		return true
+	} else {
+		return false
+	}
+}
+
+func (l *List) InsertNodeValueBack(dest interface{}, node *Node) bool {
+	panic("implement me")
 }
 
 func (l *List) GetFirstNode() *Node {
--- code/ch3/SingleLink/exe/main.go
+++ code/ch3/SingleLink/exe/main.go
@@ -11,10 +11,12 @@ func main() {
 	node2 := SingleLink.NewNode(2)
 	node3 := SingleLink.NewNode(3)
 	node4 := SingleLink.NewNode(4)
+	node5 := SingleLink.NewNode(5)
 	list.InsertNodeFront(node1)
 	list.InsertNodeFront(node2)
 	list.InsertNodeFront(node3)
 	list.InsertNodeBack(node4)
+	list.InsertNodeValueFront(1, node5)
 	fmt.Println("---")
 	fmt.Println(list)
 }
