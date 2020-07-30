59a7d536c35101eec8ae5ee53ed859da404ff6ea
teng <wwwsousoucom@126.com>
2020-07-30 11:52:24 +0800

    平衡二叉树 左旋与右旋

--- code/ch6/AVL/AVLTree.go
+++ code/ch6/AVL/AVLTree.go
@@ -43,6 +43,62 @@ func NewAVLTree(data interface{}, myFunc comparator) (*AVLNode, error) {
 	return NewNode(data), nil
 }
 
+// 左旋，逆时针
+func (avlNode *AVLNode) LeftRotate() *AVLNode {
+	headNode := avlNode.Right
+	avlNode.Right = headNode.Left
+	avlNode.Left = avlNode
+	// 更新高度
+	avlNode.height = Max(avlNode.Left.GetHeight(), avlNode.Right.GetHeight()) + 1
+	headNode.height = Max(headNode.Left.GetHeight(), headNode.Right.GetHeight()) + 1
+	return headNode
+}
+
+// 右旋，顺时针
+func (avlNode *AVLNode) RightRotate() *AVLNode {
+	headNode := avlNode.Left      // 保存左边节点
+	avlNode.Left = headNode.Right // 设为nil
+	headNode.Right = avlNode
+	// 更新高度
+	avlNode.height = Max(avlNode.Left.GetHeight(), avlNode.Right.GetHeight()) + 1
+	headNode.height = Max(headNode.Left.GetHeight(), headNode.Right.GetHeight()) + 1
+	return headNode
+}
+
+// 两次左旋
+// 两次右旋
+// 先左旋再右旋
+func (avlNode *AVLNode) LeftThenRightRotate() *AVLNode {
+	sonHeadNode := avlNode.Left.LeftRotate()
+	avlNode.Left = sonHeadNode
+	return avlNode.RightRotate()
+}
+
+// 先右旋再左旋
+func (avlNode *AVLNode) RightThenLeftRotate() *AVLNode {
+	sonHeadNode := avlNode.Right.RightRotate()
+	avlNode.Right = sonHeadNode
+	return avlNode.LeftRotate()
+}
+
+// 自动处理不平衡，差距为1平衡，差距为2不平衡
+func (avlNode *AVLNode) adjust() *AVLNode {
+	if avlNode.Right.GetHeight()-avlNode.Left.GetHeight() == 2 {
+		if avlNode.Right.Right.GetHeight() > avlNode.Right.Left.GetHeight() {
+			avlNode = avlNode.LeftRotate()
+		} else {
+			avlNode = avlNode.RightThenLeftRotate()
+		}
+	} else if avlNode.Left.GetHeight()-avlNode.Right.GetHeight() == 2 {
+		if avlNode.Left.Left.GetHeight() > avlNode.Left.Right.GetHeight() {
+			avlNode = avlNode.RightRotate()
+		} else {
+			avlNode = avlNode.LeftThenRightRotate()
+		}
+	}
+	return avlNode
+}
+
 func (avlNode *AVLNode) Find(data interface{}) *AVLNode {
 	var result *AVLNode
 	switch compare(data, avlNode.Data) {
