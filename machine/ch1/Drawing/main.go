2f5b109fe88c92a8c184dd27bc563422028fd9d0
teng <wwwsousoucom@126.com>
2020-08-05 20:41:02 +0800

    生成绘图

---
+++ machine/ch1/Drawing/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/plotutil"
+	"gonum.org/v1/plot/vg"
+)
+
+func main() {
+	p, _ := plot.New()
+
+	p.Title.Text = "Hello Price"
+	p.X.Label.Text = "Quantity Demand"
+	p.Y.Label.Text = "Price"
+
+	points := plotter.XYs{
+		{2.0, 60000.0},
+		{4.0, 40000.0},
+		{6.0, 30000.0},
+		{8.0, 25000.0},
+		{10.0, 23000.0},
+	}
+
+	plotutil.AddLinePoints(p, points)
+
+	p.Save(4*vg.Inch, 4*vg.Inch, "price.png")
+
+}
