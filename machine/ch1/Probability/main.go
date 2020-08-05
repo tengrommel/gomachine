8ba375dde41ae28601defa0193bf52cf6aa9b3f2
teng <wwwsousoucom@126.com>
2020-08-05 18:19:34 +0800

    a

--- machine/ch1/Probability/main.go
+++ machine/ch1/Probability/main.go
@@ -6,17 +6,19 @@ import (
 	"github.com/gonum/stat/distuv"
 )
 
+// 对概率的计算
 func main() {
 	//ob := []float64{48, 52}
 	//ex := []float64{50, 50}
 	//fmt.Println(stat.ChiSquare(ob, ex))
-	ob := []float64{300, 200, 100}
-	//total := 600
-	ex := []float64{0.5, 0.3, 0.2}
-	fmt.Println(stat.ChiSquare(ob, ex))
+	ob := []float64{260, 135, 105}
+	total := 500.0
+	ex := []float64{total * 0.6, total * 0.25, total * 0.15}
+	chi := stat.ChiSquare(ob, ex)
+	fmt.Println(total, chi)
 	chiDist := distuv.ChiSquared{
 		K:   2.0,
 		Src: nil,
 	}
-	fmt.Println(chiDist.Prob(stat.ChiSquare(ob, ex)))
+	fmt.Println(chiDist.Prob(chi))
 }
