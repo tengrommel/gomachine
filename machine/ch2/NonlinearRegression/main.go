7d4e193c9bd217cbd27505c1962cdcb0ddaffea0
teng <wwwsousoucom@126.com>
2020-08-07 17:13:43 +0800

    读取数据

--- machine/ch2/NonlinearRegression/main.go
+++ machine/ch2/NonlinearRegression/main.go
@@ -1,5 +1,56 @@
 package main
 
-func main() {
+import (
+	"encoding/csv"
+	"fmt"
+	"github.com/gonum/matrix/mat64"
+	"os"
+	"strconv"
+)
 
+// 非线性回归
+func main() {
+	path := "/home/teng/Documents/git/gomachine/machine/ch2/LinearRegression/Advertising.csv"
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+	reader := csv.NewReader(file)
+	reader.FieldsPerRecord = 4         // 字段
+	trainData, err := reader.ReadAll() // 读取全部
+	if err != nil {
+		fmt.Println(err)
+	}
+	featureData := make([]float64, 4*len(trainData)) // 特征数据
+	yData := make([]float64, len(trainData))
+	var featureIndex int
+	var yIndex int
+	for idx, record := range trainData {
+		if idx == 0 {
+			continue
+		}
+		for i, val := range record {
+			valParsed, err := strconv.ParseFloat(val, 64)
+			if err != nil {
+				fmt.Println(err)
+			}
+			if i < 3 {
+				if i == 0 {
+					featureData[featureIndex] = 1
+					featureIndex++
+				}
+				featureData[featureIndex] = valParsed
+				featureIndex++
+			}
+			if i == 3 {
+				yData[yIndex] = valParsed
+				yIndex++
+			}
+		}
+	}
+	features := mat64.NewDense(len(trainData), 4, featureData) // 创建矩阵
+	y := mat64.NewVector(len(trainData), yData)                // 创建向量
+	fmt.Println(features)
+	fmt.Println(y)
 }
