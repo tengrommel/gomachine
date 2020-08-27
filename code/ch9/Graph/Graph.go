5466c75d32651c0068a410992a000be98ad8b014
teng <wwwsousoucom@126.com>
2020-08-27 12:52:35 +0800

    图的实现

--- code/ch9/Graph/Graph.go
+++ code/ch9/Graph/Graph.go
@@ -25,13 +25,129 @@ type Graph interface {
 	AllVertices() []interface{} // 返回所有的顶点
 	AllEdges() []Edge           // 返回所有的边长
 
-	AllConnectedVertices() []interface{} // 返回所有的联通的顶点
-	AllConnectedEdges() []Edge           // 返回所有的联通的边长
-	Transpose() Graph                    // 返回旋转图
+	AllConnectedVertices(interface{}) []interface{} // 返回所有的联通的顶点
+	IterConnectedEdges(interface{}) []Edge          // 返回所有的联通的边长
+	Transpose() Graph                               // 返回旋转图
 }
 
 // 邻接矩阵 图的基本结构
 type AdjacencyMatrix struct {
 	matrix LinkedMap
 	Graph
+}
+
+// 构建图的数据结构并初始化
+func (g *AdjacencyMatrix) init() *AdjacencyMatrix {
+	g.matrix = *new(LinkedMap).init() // 初始化
+	return g
+}
+
+// 增加定点
+func (g *AdjacencyMatrix) AddVertex(vertex interface{}) {
+	if g.matrix.exist(vertex) {
+		g.matrix.add(vertex, new(LinkedMap).init())
+	}
+}
+
+// 判断定点是否存在
+func (g *AdjacencyMatrix) CheckVertex(vertex interface{}) bool {
+	return g.matrix.exist(vertex)
+}
+
+// 删除定点
+func (g *AdjacencyMatrix) DeleteVertex(vertex interface{}) {
+	g.matrix.delete(vertex) // 删除定点
+	// 循环删除每个边长
+	for v := g.matrix.frontKey(); v != nil; v = g.matrix.nextKey(v) {
+		g.DeleteEdge(Edge{v, vertex}) // 删除相关的边长
+	}
+}
+
+// 增加边长
+func (g *AdjacencyMatrix) AddEdge(edge Edge) {
+	g.AddVertex(edge.Start)
+	g.AddVertex(edge.End)
+	g.matrix.get(edge.Start).(*LinkedMap).add(edge.End, true)
+	// 增加边长
+}
+
+// 判断边长是否联通
+func (g *AdjacencyMatrix) CheckEdge(edge Edge) bool {
+	if !g.CheckVertex(edge.Start) {
+		return false
+	}
+	return g.matrix.get(edge).(*LinkedMap).exist(edge.End)
+	// 返回边长是否存在
+}
+
+// 删除边长
+func (g *AdjacencyMatrix) DeleteEdge(edge Edge) {
+	if g.matrix.exist(edge.Start) {
+		g.matrix.get(edge.Start).(*LinkedMap).delete(edge.End)
+	}
+}
+
+// 增加边长
+func (g *AdjacencyMatrix) AddEdgeBi(edge Edge) {
+	g.AddVertex(edge.Start)
+	g.AddVertex(edge.End)
+	g.matrix.get(edge.Start).(*LinkedMap).add(edge.End, true)
+	g.matrix.get(edge.End).(*LinkedMap).add(edge.Start, true)
+}
+
+// 删除边长
+func (g *AdjacencyMatrix) DeleteEdgeBi(edge Edge) {
+	g.DeleteEdge(edge)
+	g.DeleteEdge(Edge{edge.End, edge.Start})
+}
+
+func (g *AdjacencyMatrix) AllVertices() []interface{} {
+	keys := make([]interface{}, 0, 0)
+	for v := g.matrix.frontKey(); v != nil; v = g.matrix.nextKey(v) {
+		keys = append(keys, v)
+	}
+	return keys
+}
+
+// 没有联通的边长也算
+func (g *AdjacencyMatrix) AllEdges() []Edge {
+	edges := make([]Edge, 0, 0)
+	for start := g.matrix.frontKey(); start != nil; start = g.matrix.nextKey(start) {
+		for end := g.matrix.get(start).(*LinkedMap).frontKey(); end != nil; end = g.matrix.get(start).(*LinkedMap).nextKey(end) {
+			edges = append(edges, Edge{Start: start, End: end})
+		}
+	}
+	return edges
+}
+
+func (g *AdjacencyMatrix) AllConnectedVertices(v interface{}) []interface{} {
+	keys := make([]interface{}, 0, 0)
+	if g.matrix.exist(v) {
+		for v := g.matrix.frontKey(); v != nil; v = g.matrix.get(v).(*LinkedMap).nextKey(v) {
+			keys = append(keys, v)
+		}
+	}
+	return keys
+}
+
+func (g *AdjacencyMatrix) IterConnectedEdges(v interface{}) []Edge {
+	if g.matrix.exist(v) {
+		return nil // 循环工具
+	} else {
+		return nil
+	}
+}
+
+// 返回旋转图
+func newGraph() Graph {
+	return new(AdjacencyMatrix).init()
+}
+
+// 返回镜像图
+func (g *AdjacencyMatrix) Transpose() Graph {
+	gt := newGraph()
+	for _, e := range g.AllEdges() {
+		gt.AddEdge(Edge{e.End, e.Start})
+	}
+	return gt
 }
--- code/ch9/Graph/LinkedMap.go
+++ code/ch9/Graph/LinkedMap.go
@@ -6,3 +6,63 @@ type LinkedMap struct {
 	keyL  *list.List                  // 链表
 	myMap map[interface{}]interface{} // 映射关系
 }
+
+// 数据结构的
+func (lm *LinkedMap) init() *LinkedMap {
+	lm.keyL = list.New()
+	lm.myMap = make(map[interface{}]interface{})
+	return lm
+}
+
+func (lm *LinkedMap) exist(key interface{}) bool {
+	_, ok := lm.myMap[key]
+	return ok
+}
+
+// 插入数据， key value
+func (lm *LinkedMap) add(key interface{}, value interface{}) {
+	if !lm.exist(key) {
+		e := lm.keyL.PushBack(key) // 压入数据
+		lm.myMap[key] = []interface{}{e, value}
+	} else {
+		lm.myMap[key].([]interface{})[1] = value
+	}
+}
+
+// 抓取数据
+func (lm *LinkedMap) get(key interface{}) interface{} {
+	if lm.exist(key) {
+		return lm.myMap[key].([]interface{})[1]
+	} else {
+		return nil
+	}
+}
+
+func (lm *LinkedMap) delete(key interface{}) {
+	if lm.exist(key) {
+		i := lm.myMap[key].([]interface{})
+		lm.keyL.Remove(i[0].(*list.Element))
+		delete(lm.myMap, key) // 删除
+	}
+}
+
+func (lm *LinkedMap) frontKey() interface{} {
+	if lm.keyL.Len() == 0 {
+		return nil
+	}
+	return lm.keyL.Front().Value
+}
+
+func (lm *LinkedMap) nextKey(key interface{}) interface{} {
+	if e := lm.myMap[key].([]interface{})[0].(*list.Element).Next(); e != nil {
+		return e.Value
+	}
+	return nil
+}
+
+func (lm *LinkedMap) prevKey(key interface{}) interface{} {
+	if e := lm.myMap[key].([]interface{})[0].(*list.Element).Prev(); e != nil {
+		return e.Value
+	}
+	return nil
+}
